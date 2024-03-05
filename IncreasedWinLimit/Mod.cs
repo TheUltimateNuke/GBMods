cc3a89fb026d4cbea00a03ba3838fe3a64b533d4
The_UltimateNuke <84410206+TheUltimateNuke@users.noreply.github.com>
2024-03-05 15:41:50 -0500

    added IncreasedWinLimit source code

---
+++ IncreasedWinLimit/Mod.cs
@@ -0,0 +1,27 @@
+﻿using MelonLoader;
+
+namespace IncreasedWinLimit
+{
+    public static class BuildInfo
+    {
+        public const string Name = "IncreasedWinLimit";
+        public const string Author = "TheUltimateNuke";
+        public const string Description = null;
+        public const string Company = null;
+        public const string Version = "1.0.0";
+        public const string DownloadLink = null;
+    }
+
+    public class Mod : MelonMod
+    {
+        public static int WinLimit => _winLimit.Value;
+
+        private static readonly MelonPreferences_Category _melonCat = MelonPreferences.CreateCategory(BuildInfo.Name);
+        private static readonly MelonPreferences_Entry<int> _winLimit = _melonCat.CreateEntry(nameof(_winLimit), 15, "Win Limit");
+
+        public override void OnInitializeMelon()
+        {
+            HarmonyInstance.PatchAll();
+        }
+    }
+}
---
+++ IncreasedWinLimit/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using MelonLoader;
+
+[assembly: AssemblyTitle(IncreasedWinLimit.BuildInfo.Description)]
+[assembly: AssemblyDescription(IncreasedWinLimit.BuildInfo.Description)]
+[assembly: AssemblyCompany(IncreasedWinLimit.BuildInfo.Company)]
+[assembly: AssemblyProduct(IncreasedWinLimit.BuildInfo.Name)]
+[assembly: AssemblyCopyright("Created by " + IncreasedWinLimit.BuildInfo.Author)]
+[assembly: AssemblyTrademark(IncreasedWinLimit.BuildInfo.Company)]
+[assembly: AssemblyVersion(IncreasedWinLimit.BuildInfo.Version)]
+[assembly: AssemblyFileVersion(IncreasedWinLimit.BuildInfo.Version)]
+[assembly: MelonInfo(typeof(IncreasedWinLimit.Mod), IncreasedWinLimit.BuildInfo.Name, IncreasedWinLimit.BuildInfo.Version, IncreasedWinLimit.BuildInfo.Author, IncreasedWinLimit.BuildInfo.DownloadLink)]
+[assembly: MelonColor(255, 222, 103, 84)]
+
+[assembly: MelonGame("Boneloaf", "Gang Beasts")]
